140ebc119c850bf433bc8f917ed260bcb5225345
Ayush Gupta <ayushgupta3902@gmail.com>
2024-01-12 23:51:58 +0530

    fix: add cursor pointer to FormSelect.jsx

--- src/components/dashboard/FormSelect.jsx
+++ src/components/dashboard/FormSelect.jsx
@@ -1,12 +1,12 @@
 function FormSelect({ label, placeholder, name, required, options }) {
   return (
-    <div className="m-2">
+    <div className="m-2 cursor-pointer">
       <label className="font-medium text-xs" htmlFor={name}>
         {label}
         {required && <sup className="text-red-500 text-sm y-5">*</sup>}
       </label>
       <select
-        className="p-2 placeholder:font-normal bg-[#ECF1F4] shadow-md w-full rounded-md"
+        className="p-2 cursor-pointer placeholder:font-normal bg-[#ECF1F4] shadow-md w-full rounded-md"
         name={name}
         id={name}
         required={required}
